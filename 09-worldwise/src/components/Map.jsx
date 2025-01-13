0844c9471833cc1d401313077875945ef8e63fcd
0xmhc <mohcinejawad783@gmail.com>
2025-01-13 17:15:19 +0100

    add hook useSearchParams for get queries

--- 09-worldwise/src/components/Map.jsx
+++ 09-worldwise/src/components/Map.jsx
@@ -1,6 +1,23 @@
+import { useSearchParams } from "react-router-dom";
 import styles from "./Map.module.css";
 function Map() {
-  return <div className={styles.mapContainer}>Map</div>;
+  const [searchParams, setSearchPararms] = useSearchParams();
+  const lat = searchParams.get("lat");
+  const lng = searchParams.get("lng");
+  return (
+    <div className={styles.mapContainer}>
+      <h1>
+        Position: {lat}, {lng}
+      </h1>
+      <button
+        onClick={() => {
+          setSearchPararms({ lat: 222, lng: 21 });
+        }}
+      >
+        change pos
+      </button>
+    </div>
+  );
 }
 
 export default Map;
