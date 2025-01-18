93d39e070cddc01cdc12766ebba9c92ab515e75b
0xmhc <mohcinejawad783@gmail.com>
2025-01-18 16:33:53 +0100

    create context for the user

--- 09-worldwise/src/App.jsx
+++ 09-worldwise/src/App.jsx
@@ -10,7 +10,6 @@ import CityList from "./components/CityList";
 import CountryList from "./components/CountryList";
 import City from "./components/City";
 import Form from "./components/Form";
-
 import { CitiesProvider } from "./contexts/CitiesContext";
 
 function App() {
