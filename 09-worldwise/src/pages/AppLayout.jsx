280cc7fee7ce59a99a5dd558f1afbc8f0d4104c2
0xmhc <mohcinejawad783@gmail.com>
2025-01-18 17:14:29 +0100

    import fakeAuth to the User.jsx and logout

--- 09-worldwise/src/pages/AppLayout.jsx
+++ 09-worldwise/src/pages/AppLayout.jsx
@@ -1,12 +1,16 @@
 import Map from "../components/Map";
 import Sidebar from "../components/Sidebar";
 import styles from "./AppLayout.module.css";
+import { useAuth } from "../contexts/FakeAuthContext";
+import { useNavigate } from "react-router-dom";
+import User from "../components/User";
 
 function AppLayout() {
   return (
     <div className={styles.app}>
       <Sidebar />
       <Map />
+      <User />
     </div>
   );
 }
