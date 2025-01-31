c3aafab2c0d504cc337bdecaf81c3076f323af2a
0xmhc <mohcinejawad783@gmail.com>
2025-01-31 08:38:02 +0100

    make some changes

--- 13-fast-react-pizza/src/ui/AppLayout.jsx
+++ 13-fast-react-pizza/src/ui/AppLayout.jsx
@@ -2,7 +2,6 @@ import { Outlet, useNavigation } from "react-router-dom";
 import Header from "./Header";
 import CartOverview from "../features/cart/CartOverview";
 import Loader from "./Loader";
-
 function AppLayout() {
   const navigation = useNavigation();
   const isLoading = navigation.state === "loading";
