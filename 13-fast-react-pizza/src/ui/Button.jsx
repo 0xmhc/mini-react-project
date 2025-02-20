7f87f6d5373f0ca681ebcd5a4c122b76ba216f35
0xmhc <mohcinejawad783@gmail.com>
2025-02-20 10:16:12 +0100

    add props to the Link

--- 13-fast-react-pizza/src/ui/Button.jsx
+++ 13-fast-react-pizza/src/ui/Button.jsx
@@ -9,7 +9,12 @@ function Button({ children, disabled, to, type }) {
     secondary:
       "inline-block text-sm rounded-full border-2 border-stone-300 font-semibold uppercase tracking-wide text-stone-400 transition-colors duration-300 hover:bg-stone-300 hover:text-stone-800 focus:bg-stone-300 focus:text-stone-800 focus:outline-none focus:ring focus:ring-stone-200 focus:ring-offset-2 disabled:cursor-not-allowed px-4 py-2.5 md:px-6 md:py-3.5",
   };
-  if (to) return <Link className={styles[type]}>{children}</Link>;
+  if (to)
+    return (
+      <Link className={styles[type]} to={to}>
+        {children}
+      </Link>
+    );
   return (
     <button className={styles[type]} disabled={disabled}>
       {children}
