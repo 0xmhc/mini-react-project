65c79450d89876f44b0d288114f5ab0b4a3848a0
0xmhc <mohcinejawad783@gmail.com>
2025-02-28 15:52:59 +0000

    add heading component and pass props as to heading style

--- 14-the-wild-oasis/src/App.jsx
+++ 14-the-wild-oasis/src/App.jsx
@@ -1,27 +1,27 @@
 import styled from "styled-components";
 import GlobalStyles from "./styles/GlobalStyles";
-import Button from "./ui/Button"
-import Input from "./ui/Input"
-const H1 = styled.h1`
-    font-size:30px;
-    font-weight:600;
-    background-color: yellow;
-`
+import Button from "./ui/Button";
+import Input from "./ui/Input";
+import Heading from "./ui/Heading";
 
 const StyledApp = styled.div`
-    background-color: red;
-    padding:20px;
-`
+  background-color: red;
+  padding: 20px;
+`;
 function App() {
-  return <>
-  <GlobalStyles/>
-  <StyledApp>
-    <H1>The Wild Oasis</H1>
-    <Button>Check In</Button>
-    <Input type="number" placeholder='number of guests'/>
-  </StyledApp>;
-  </>
+  return (
+    <>
+      <GlobalStyles />
+      <StyledApp>
+        <Heading as="h1">The Wild Oasis</Heading>
+        <Heading as="h2">Check In and out</Heading>
+        <Button>Check In</Button>
+        <Input type="number" placeholder="number of guests" />
+        <Heading as="h3">Forms</Heading>
+      </StyledApp>
+      ;
+    </>
+  );
 }
 
 export default App;
- 
